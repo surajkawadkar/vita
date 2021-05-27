831c1462ba17764b82943bfa1d661ae913d2da2b
Suraj Kawadkar <suraj.kawadkar14@gmail.com>
2021-05-27 12:26:39 +0530

    program with wrong error

---
+++ JAVA/day 1/day 2/nullexc.java
@@ -0,0 +1,14 @@
+class a{
+    void display(){
+
+    System.out.println("fjaklds"); }
+   
+}
+
+class nullexc{
+    public static void main(String args[]){
+        a s1 = new a();
+        s1=null;
+        s1.disp();
+    }
+}
