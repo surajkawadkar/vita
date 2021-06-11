05bb962a1170f63ca615b806c07eabaab2ad2eb0
Suraj Kawadkar <suraj.kawadkar14@gmail.com>
2021-06-11 23:17:18 +0530

    day 1 logical program added

---
+++ PYTHON/snippet/day_1.py
@@ -0,0 +1,50 @@
+
+# Some of the formulas used in today's Lab Session :
+# Simple Interest= (Rate of Interest* Principal Amount*Time)/100
+# Total Amount = Principal Amount+ Simple Interest
+# 1 Feet = 12 inches
+# 1 Inch= 2.5 cm
+# 1 Hour= 60 Minutes
+# 1 Minute = 60 Seconds
+# therefore 1 Hour= 60* 60 Seconds
+
+
+# inr =float(input())
+# dollars = inr/73.05
+# print(dollars)
+
+# # total returm of  FD
+# amt=float(input("amt"))
+# interest=int(input("year"))
+# ror=float(input("ror"))
+# print("return is",(amt*interest*ror)+amt)
+
+
+# total sec 
+# hour=int(input("hours"))
+# min=int(input("min"))
+# sec=int(input("sec"))
+# print("total sec =",(hour*60*60)+(min*60)+sec)
+
+# seconds to hour min and sec
+# sec=int(input("sec"))
+# hour=sec//3600
+# min=(sec%3600)//60
+# seconds=sec-(sec//3600)-(sec+(sec//3600))
+# print("total sec =",hour,min,seconds)
+
+
+# cm to foo and inch
+# height=float(input("enter in cm"))
+# print("foot =",height//(2.5*12))
+# print("inch",(height%12)//2.5)
+
+
+# height=float(input("enter in cm"))
+# feet=height//(2.54*12)
+# print("foot =",feet)
+# inch=(height%12)%2.5
+# print("inch",inch)
+
+# convet height into cm
+print("Height in cm is:",float(input("Enter height:"))*12*2.5)
