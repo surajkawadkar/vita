62b15b68762394c494bcecd4258234265e811703
Suraj Kawadkar <suraj.kawadkar14@gmail.com>
2021-06-11 14:51:14 +0530

    python basic in vita

---
+++ PYTHON/snippet/hello.py
@@ -0,0 +1,17 @@
+# from typing import TYPE_CHECKING
+
+
+a=45
+
+print(type(a))
+a,b=1,'suraj'
+print(a,b)
+a,b=b,a
+print(a,b)
+
+
+# name=input("enter the name")
+# print(name,end='.')
+import  math
+ceiling=math.ceil(25.2)
+print(ceiling)
