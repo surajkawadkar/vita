9fcd177dd07b49dd49120d5d84f5497bb981d13c
Suraj Kawadkar <suraj.kawadkar14@gmail.com>
2021-06-12 16:11:48 +0530

    quadratic root in pythion not working state

---
+++ PYTHON/Assignment_1/quadratic.py
@@ -0,0 +1,16 @@
+import math
+a=int(input("enter x^2 term"))
+b= int(input("enter bx term"))
+c=int(input("neter conmstant term"))
+print("expression is",a,'x^2',b'x +',c)
+nume=(b*b-4*a*c)
+den= (2*a)
+print(nume)
+print(den)
+
+
+r1=-b+(math.sqrt(nume/den))
+r2=-b-((math.sqrt(nume/den)))
+print(r1)
+print(r2)
+
