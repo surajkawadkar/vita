4d402467b34f016ea6c5ba75e04594512c338ebe
Suraj Kawadkar <suraj.kawadkar14@gmail.com>
2021-06-12 21:08:18 +0530

    descending order among 3 number without using loop

--- PYTHON/Assignment_1/num_sort.py
+++ PYTHON/Assignment_1/num_sort.py
@@ -1,6 +1,21 @@
-num1= int(input("eneer first number"))
-num2= int(input("eneer second number"))
-if num1<num2:
-    print("assending order is",num1,num2)
+num1= int(input("enter first number"))
+num2= int(input("enter second number"))
+num3= int(input("enter third number"))
+condition1=num3>num2>num1
+condition2=num3>num1>num2
+condition3=num2>num1>num3
+condition4=num2>num3>num1
+condition5=num1>num3>num2
+if condition1:
+    print("descending  order is",num3,num2,num1)
+elif condition2:
+    print("descending order is",num3,num1,num2)
+elif condition3:
+    print("descending order is",num2,num1,num3)
+elif condition4:
+    print("descending order is",num2,num3,num1)
+elif condition5:
+    print("descending order is",num1,num3,num2)
+
 else:
-    print("assending order is",num2,num1)
+    print("descending order is",num1,num2,num3)
