e434de68f46dd4893dcbc25f6238100ae340f2eb
Suraj Kawadkar <suraj.kawadkar14@gmail.com>
2021-06-12 20:47:37 +0530

    largest among 3

---
+++ PYTHON/Assignment_1/largest.py
@@ -0,0 +1,15 @@
+num1=int(input("Enter 1st number"))
+num2=int(input("Enter 2st number"))
+num3=int(input("Enter 3st number"))
+if(num1>num2 and num1>num3):
+    print(num1," is the largest")
+elif (num2>num1 and num2>num3):
+    print(num2,'is the largest number')
+else:
+     print(num3,'is the largest number')
+
+
+
+
+
+
