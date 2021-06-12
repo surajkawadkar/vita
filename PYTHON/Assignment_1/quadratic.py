58faa2787bd4225713951bdc752103d8eec4b619
Suraj Kawadkar <suraj.kawadkar14@gmail.com>
2021-06-12 18:59:28 +0530

    typo fixes

--- PYTHON/Assignment_1/quadratic.py
+++ PYTHON/Assignment_1/quadratic.py
@@ -2,7 +2,7 @@ import math
 a=int(input("enter x^2 term"))
 b= int(input("enter bx term"))
 c=int(input("neter conmstant term"))
-print("expression is",a,'x^2',b'x +',c)
+print("expression is",a,'x^2'+'+',b,'x +',c)
 nume=(b*b-4*a*c)
 den= (2*a)
 print(nume)
