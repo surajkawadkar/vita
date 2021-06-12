d16d5d5064f7e01b783daae5a41a455dde18b8d3
Suraj Kawadkar <suraj.kawadkar14@gmail.com>
2021-06-12 11:08:24 +0530

    grade of a student

--- PYTHON/day_2/day_2.py
+++ PYTHON/day_2/day_2.py
@@ -1,12 +1,23 @@
 # minmum number
-num1=int(input())
-num2=int(input())
-num3=int(input())
-if(num1<num2 and num2<num3):
-    print(num1)
+# num1=int(input())
+# num2=int(input())
+# num3=int(input())
+# if(num1<num2 and num2<num3):
+#     print(num1)
 
-elif (num2<num3 and num2<num1):
-    print(num2)
+# elif (num2<num3 and num2<num1):
+#     print(num2)
+# else:
+#     print(num3)
+
+# grades of a student
+perc=float(input())
+if perc>60:
+    print("first class")
+elif perc>=50:
+    print("second class")
+elif perc>=40:
+    print("pass")
 else:
-    print(num3)
+    print("fail")
 
