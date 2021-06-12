cdbc98708a78a770e5344ebf09b8637c19f4ba9b
Suraj Kawadkar <suraj.kawadkar14@gmail.com>
2021-06-12 18:00:43 +0530

    2 number put in assending order

---
+++ PYTHON/Assignment_1/num_sort.py
@@ -0,0 +1,6 @@
+num1= int(input("eneer first number"))
+num2= int(input("eneer second number"))
+if num1<num2:
+    print("assending order is",num1,num2)
+else:
+    print("assending order is",num2,num1)
