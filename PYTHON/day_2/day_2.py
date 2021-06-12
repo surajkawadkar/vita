4b7136e064a2cad901f74de6151586ae15b6c015
Suraj Kawadkar <suraj.kawadkar14@gmail.com>
2021-06-12 13:03:20 +0530

    sum of digit

--- PYTHON/day_2/day_2.py
+++ PYTHON/day_2/day_2.py
@@ -21,7 +21,40 @@
 # else:
 #     print("fail")
 
-for i in range(1,10,1):
-    if i==3:
-        break
-    print(i)
+# for i in range(1,10,1):
+#     if i==3:
+#         break
+#     print(i)
+
+# dicisible by 5 and 3
+# for i in range(1,101,3):
+#     if(i%5==0):
+#         print(i)
+
+# accept input from user till user does not enter nothing and hit enter and find sum and avg
+# sum=0
+# while(1):
+#     x=input()
+#     if x='':
+#         break
+
+    
+#     sum+=float(x)
+# print(sum)
+# print(sum/num)
+
+# Factorial
+# fact=1
+# num=int(input())
+# for i in range(1,num+1):
+#     fact*=i
+# print(fact)
+
+num= int(input())
+sum=0
+while(num>0):
+    rem=num%10
+    sum+=rem
+    num=num//10
+print(sum)
+
