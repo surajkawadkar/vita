2ba3b2fcd99fd97516e183d44e8ea309e58c7ece
Suraj Kawadkar <suraj.kawadkar14@gmail.com>
2021-06-13 00:27:37 +0530

    map function working implimented

---
+++ PYTHON/Assignment_1/map_function.py
@@ -0,0 +1,4 @@
+# when i want to use multiple input integet or float then it is more usefull
+# by default split() is applicable to string
+num1,num2= map(float,input("real then natural").split())
+print(num1**num2)
