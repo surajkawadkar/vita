57efda5324db3c03634faccfc2a9ce0e7e8b9fa6
Suraj Kawadkar <suraj.kawadkar14@gmail.com>
2021-06-15 10:36:41 +0530

    mirror and pyramid

--- PYTHON/day4.py
+++ PYTHON/day4.py
@@ -1,2 +1,13 @@
+# for i in range(1,6,1):
+#     print('*'*i)
+
+
+# for i in range(5,0,-1):
+#     print('*'*i)
+# mirror pattern
 for i in range(1,6,1):
-    print('*'*i)
+  print(' '*(6-i),'x'*i)
+
+# pyramid
+for i in range(1,6,1):
+  print(' '*(6-i),' x'*i)
