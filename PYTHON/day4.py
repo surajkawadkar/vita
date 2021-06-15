408659cf48a9abe8e4f600e0dc286d026d8f704d
Suraj Kawadkar <suraj.kawadkar14@gmail.com>
2021-06-15 10:48:05 +0530

    word pyramid

--- PYTHON/day4.py
+++ PYTHON/day4.py
@@ -5,9 +5,17 @@
 # for i in range(5,0,-1):
 #     print('*'*i)
 # mirror pattern
-for i in range(1,6,1):
-  print(' '*(6-i),'x'*i)
+# for i in range(1,6,1):
+#   print(' '*(6-i),'x'*i)
 
-# pyramid
-for i in range(1,6,1):
-  print(' '*(6-i),' x'*i)
+# # pyramid
+# for i in range(1,6,1):
+#   print(' '*(6-i),' x'*i)
+
+# for i in range(5,0,-2):
+#     print(' '*(6-i),' x'*i)
+
+# word pyramid
+name='suraj'
+for i in range(0,len(name)+1):
+    print(name[:i+1])
