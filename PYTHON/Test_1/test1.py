e95151353ba9666d20acdd88c1f347f173bbeedd
Suraj Kawadkar <suraj.kawadkar14@gmail.com>
2021-06-17 18:48:37 +0530

    count digit, revese list and slicing accomplished

---
+++ PYTHON/Test_1/test1.py
@@ -0,0 +1,52 @@
+# Given a number count the total number of digits in a number
+# For example: The number is 75869, so the output should be
+# num=75869
+# count=0
+# while(num>0):
+#     num%10
+#     count+=1
+#     num=num//10
+# print(count)
+
+
+
+
+# Reverse the following list using for loop 
+# list1 = [10, 20, 30, 40, 50] 
+# Expected output: 
+# 50 
+# 40 
+# 30 
+# 20 
+# 10 
+
+# list1 = [10, 20, 30, 40, 50] 
+# for i in range(-1,-len(list1),-1):
+#     print(list1[i])
+
+
+
+
+
+
+# 2. Given 2 strings, s1 and s2, create a new string by appending s2 in the middle of s1 Expected Outcome: 
+# appendMiddle("Chrisdem", IamNewString) → "ChrIamNewStringisdem" 
+
+# s1='Chrisdem'
+# s2='IamNewString'
+# l_s1=len(s1)//2
+# print(s1[:l_s1]+s2+s1[l_s1:])
+
+
+
+
+
+
+#  Arrange String characters such that lowercase letters should come first 
+# Given input String of combination of the lower and upper case arrange characters in such a way that all lowercase letters should come first. 
+# Expected Output: 
+# input_String = PyNaTive 
+# arranging characters giving precedence to lowercase letters 
+# aeiNPTvy 
+# arranging characters giving precedence to lowercase letters: 
+# yaivePNT 
