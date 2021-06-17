82ff12a30ac029806996e93b312cf1a066c5e155
Suraj Kawadkar <suraj.kawadkar14@gmail.com>
2021-06-17 17:20:45 +0530

    31.gcd of 2 number

---
+++ PYTHON/Exercise/125 exercise.py
@@ -0,0 +1,24 @@
+# 29. Write a Python program to print out a set containing all the colors from color_list_1 which are not present in color_list_2. \
+
+# list1=['a','b','c']
+# list2=['a','r','d']
+# for i in list1:
+#     if i not in list2:
+#         print(i)
+
+# 31. Write a Python program to compute the greatest common divisor (GCD) of two positive integers
+
+# num1= int(input())
+# num2= int(input())
+# i=1
+# if num1>num2:
+#     large=num1
+# else:
+#     large=num2
+# for i in range(1,large):
+#     if num1%i==0 and num2%i==0:
+#         gcd=i
+
+# print(gcd)
+
+            
