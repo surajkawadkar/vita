448e7a1889f57112fe72456d6f78b14d5f6a05fb
Suraj Kawadkar <suraj.kawadkar14@gmail.com>
2021-06-17 15:01:45 +0530

    reverse order name and surname with space

--- PYTHON/Exercise/one_ten.py
+++ PYTHON/Exercise/one_ten.py
@@ -8,5 +8,22 @@
 # print(datetime.datetime.now())
 
 
-r= int(input())
-print(2*3.14*r*r)
+# 4. Write a Python program which accepts the radius of a circle from the user and compute the area. 
+# Sample Output :
+# r = 1.1
+# Area = 3.8013271108436504
+
+
+# r= int(input())
+# print(2*3.14*r*r)
+
+
+# 5. Write a Python program which accepts the user's first and last name and print 
+# them in reverse order with a space between them. 
+name=input()
+surname=input()
+newstr=name+' '+surname
+result=''
+for i in newstr:
+    result=i+result
+print(result)
