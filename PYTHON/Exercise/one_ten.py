94bdd862f23a1f637ac28970e75d9ce276b56170
Suraj Kawadkar <suraj.kawadkar14@gmail.com>
2021-06-17 15:09:11 +0530

    data printed in list and tuple form

--- PYTHON/Exercise/one_ten.py
+++ PYTHON/Exercise/one_ten.py
@@ -20,10 +20,24 @@
 
 # 5. Write a Python program which accepts the user's first and last name and print 
 # them in reverse order with a space between them. 
-name=input()
-surname=input()
-newstr=name+' '+surname
-result=''
-for i in newstr:
-    result=i+result
-print(result)
+# name=input()
+# surname=input()
+# newstr=name+' '+surname
+# result=''
+# for i in newstr:
+#     result=i+result
+# print(result)
+
+# 6. Write a Python program which accepts a sequence of comma-separated numbers from user and generate a list and a tuple with those numbers. 
+# Sample data : 3, 5, 7, 23
+# Output :
+# List : ['3', ' 5', ' 7', ' 23']
+# Tuple : ('3', ' 5', ' 7', ' 23')
+list1=[]
+while True:
+    data= input('enter the number')
+    if data=='':
+        break
+    list1.append(data)
+print(list1)
+print(tuple(list1))
